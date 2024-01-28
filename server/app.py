d7400545c1554df1ca8565b089c8cb2136aae673
Ruth <ruro122020@gmail.com>
2024-01-28 07:48:09 -0500

    complete

--- server/app.py
+++ server/app.py
@@ -21,6 +21,41 @@ def index():
     return make_response(body, 200)
 
 # Add views here
+@app.route('/earthquakes/<int:id>')
+def earthquakes_by_id(id):
+  earthquake = Earthquake.query.filter(Earthquake.id == id).first()
+
+  if earthquake:
+    body = earthquake.to_dict()
+    status = 200
+  else:
+    body = {
+       'message': f'Earthquake {id} not found.'
+    }
+    status = 404
+
+  return make_response(body, status)
+
+@app.route('/earthquakes/magnitude/<float:magnitude>')
+def earthquake_by_magnitude(magnitude):
+  earthquakes = []
+  
+  for earthquake in Earthquake.query.filter(Earthquake.magnitude >= magnitude):
+    earthquakes.append(earthquake.to_dict())
+
+  if earthquakes:
+    body = {
+       'count': len(earthquakes),
+       'quakes': earthquakes
+    }
+    status = 200
+  else:
+    body = {
+      'count': len(earthquakes),
+      'quakes':earthquakes
+    }
+    status = 200
+  return make_response(body, status)  
 
 
 if __name__ == '__main__':
